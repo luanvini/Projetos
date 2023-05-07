e87731d2eb1a4c3ed05e8bea664364d86b513da0
luanvini <131618478+luanvini@users.noreply.github.com>
2023-05-07 13:29:03 -0300

    Delete Envio.js

--- Envio.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("envio");
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    alert("Formulário enviado com sucesso!");
-
-    window.location.href = "agradecimento.html";
-
-});
